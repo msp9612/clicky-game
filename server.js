02d1804a190ac50d38d60b31ec568684a38d683e
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-13 03:27:33 -0500

    ESLint and Prettier

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* Dependencies */
 /* -------------------------------*/
 
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 /* Global variables and instances */
 /* -------------------------------*/
@@ -16,15 +16,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 }
 
 // Send miscellaneous requests to the React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
