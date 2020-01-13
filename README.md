32c5e1dadae846f6a4b8ba028c77efaea2df8aae
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-13 01:11:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# clicky-game
+##### Created by Matt Petrower
+
+[Text goes here]
+
+[Github repository](https://github.com/msp9612/clicky-game)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ server.js
@@ -0,0 +1,30 @@
+/* Dependencies */
+/* -------------------------------*/
+
+const express = require("express");
+const path = require("path");
+
+/* Global variables and instances */
+/* -------------------------------*/
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+/* MAIN */
+/* -------------------------------*/
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send miscellaneous requests to the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
