0c269abe54ca5c509eaa2224f59bfda77e30f5c5
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-22 21:16:56 -0500

    Remove unused index.css import

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@
 /* -------------------------------*/
 
 import React from 'react';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { render } from 'react-dom';
